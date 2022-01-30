61d7cca4e364c0bd850e42ffd3aa80391275bbae
Anthony Barragan <anthony.bar.89@gmail.com>
2022-01-29 20:21:52 -0800

    cities saved in local storage and load as buttons

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).foundation();
 const apiKey = "8b169dfd2096fa29542212c363f5739e";
 const searchEl = document.querySelector("#submit")
 const degreeSymbol = String.fromCharCode(176);
+let cityCount = localStorage.length;
 
 const getCurrentDate = function (num, elementId, long) {
     // Make date
@@ -33,7 +34,7 @@ const getUVIndex = function (lat, lon) {
             return response.json();
         })
         .then(function (data) {
-            const uvIndex = data.daily[0].uvi; 
+            const uvIndex = data.daily[0].uvi;
             if (uvIndex < 3) {
                 uvIndexEl.classList.add("label", "success");
                 uvIndexEl.textContent = uvIndex;
@@ -51,20 +52,8 @@ const getCurrentCityWeatherInfo = function (event) {
     event.preventDefault();
     // Get the input value for city
     let city = document.querySelector("#selected-city").value;
-    // Format city name for display
-    let cityFormatted = "";
-    city = city.split(" ");
-    if (city.length > 0) {
-        for (let i = 0; i < city.length; i++) {
-            cityFormatted += city[i].charAt(0).toUpperCase() + city[i].slice(1) + " ";
-            document.querySelector("#current-city").textContent = cityFormatted
-        }
-    } else {
-        cityFormatted = city.charAt(0).toUpperCase() + city.slice(1);
-        document.querySelector("#current-city").textContent = cityFormatted;
-    }
     // Make url and fetch call
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityFormatted}&appid=${apiKey}&units=imperial`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     fetch(url)
         .then(function (response) {
             return response.json()
@@ -89,6 +78,11 @@ const getCurrentCityWeatherInfo = function (event) {
             let lat = data.coord.lat;
             let lon = data.coord.lon;
             getUVIndex(lat, lon)
+            // Get city and save to local storage and display 
+            let cityName = data.name
+            document.querySelector("#current-city").textContent = cityName
+            saveCity(cityName)
+            console.log(cityName)
         })
 }
 const forecast = function (day, noonTime) {
@@ -109,14 +103,47 @@ const forecast = function (day, noonTime) {
             document.querySelector(`#wind-day-${day}`).textContent = `${wind} MPH`
             // Get and display humidity
             let humidity = data.list[noonTime].main.humidity;
-            document.querySelector(`#humidity-day-${day}`).textContent= `${humidity}%`
+            document.querySelector(`#humidity-day-${day}`).textContent = `${humidity}%`
             // Get and display icon
             let icon = data.list[noonTime].weather[0].icon;
             const iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
             document.querySelector(`#icon-day-${day}`).setAttribute("src", iconurl)
-            
         })
 }
+// LOCAL STORAGE
+const saveCity = function (city) {
+    const cityList = document.getElementById("city-list");
+    const listArray = cityList.childNodes;
+    console.log(listArray)
+    for (let i = 0; i < listArray.length; i++) {
+        if (listArray[i].innerHTML === city) {
+            return;
+        }
+    }
+    const button = document.createElement("button");
+    button.classList.add("button", "secondary");
+    button.textContent = city;
+
+    // button.onclick = searchCity
+
+    document.getElementById("city-list").appendChild(button);
+    localStorage.setItem(cityCount, city);
+    cityCount++;
+}
+const loadCities = function () {
+    for (let i = 0; i < localStorage.length; i++) {
+        const city = localStorage.getItem([i]);
+        const button = document.createElement("button");
+        button.classList.add("button", "secondary");
+        button.textContent = city;
+        document.getElementById("city-list").appendChild(button);
+    }
+}
+loadCities();
+
+// const buttonFunct = function(event) {
+//     city = this.
+// }
 
 searchEl.addEventListener("click", getCurrentCityWeatherInfo);
 searchEl.addEventListener("click", forecast.bind(event, 1, 4));
