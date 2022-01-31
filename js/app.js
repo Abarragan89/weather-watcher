b25671e4ae64882752c47cb9d823aa0ff49c38d7
Anthony Barragan <anthony.bar.89@gmail.com>
2022-01-31 13:37:03 -0800

    current location's weather is automatically displayed

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@ const apiKey = "8b169dfd2096fa29542212c363f5739e";
 const searchEl = document.querySelector("#submit")
 const buttonDivEl = document.querySelector("#city-list")
 const degreeSymbol = String.fromCharCode(176);
-let cityCount = localStorage.length;
 
-const getCurrentDate = function (num, elementId, long) {
+// Make a date with optional added days
+const getCurrentDate = function (addDays, elementId, long) {
     // Make date
     const today = new Date();
-    today.setDate(today.getDate() + num)
+    today.setDate(today.getDate() + addDays)
     const month = today.toLocaleString('defualt', { month: 'long' });
     const monthNum = today.getMonth() + 1;
     const date = today.getDate();
@@ -55,6 +55,7 @@ const getCurrentCityWeatherInfo = function (event) {
     // Make url and fetch call
     findCity(city);
 }
+
 const forecast = function (day, noonTime) {
     getCurrentDate(day, `date-${day}`);
     let city = document.querySelector("#selected-city").value;
@@ -94,8 +95,7 @@ const saveCity = function (city) {
     button.classList.add("button", "secondary");
     button.textContent = city;
     document.getElementById("city-list").appendChild(button);
-    localStorage.setItem(cityCount, city);
-    cityCount++;
+    localStorage.setItem(localStorage.length, city);
 }
 const loadCities = function () {
     for (let i = 0; i < localStorage.length; i++) {
@@ -106,7 +106,6 @@ const loadCities = function () {
         document.getElementById("city-list").appendChild(button);
     }
 }
-loadCities();
 
 const buttonSearch = function (event) {
     const city = event.target.innerHTML
@@ -119,69 +118,121 @@ const buttonSearch = function (event) {
         noonTime += 7;
     }
 }
-buttonDivEl.addEventListener("click", buttonSearch)
 
+
+
+const findCity = function (city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    fetch(url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data)
+        // Get current temperature
+        let currentTemp = data.main.temp;
+        currentTemp = currentTemp.toFixed(0);
+        document.querySelector("#current-temp").textContent = `${currentTemp}${degreeSymbol} F`;
+        // Get current wind
+        let currentWind = data.wind.speed;
+        document.querySelector("#current-wind").textContent = `${currentWind} MPH`
+        // Get current humidity
+        let currentHumidity = data.main.humidity;
+        document.querySelector("#current-humidity").textContent = `${currentHumidity}%`
+        // Get Icon
+        let icon = data.weather[0].icon;
+        const iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+        document.querySelector("#current-icon").setAttribute("src", iconurl)
+        // Get current UV Index (first get lat and lon)
+        let lat = data.coord.lat;
+        let lon = data.coord.lon;
+        getUVIndex(lat, lon)
+        // Get city and save to local storage and display 
+        let cityName = data.name
+        document.querySelector("#current-city").textContent = cityName;
+        saveCity(cityName)
+    })
+}
+
+const getForecast = function (day, city, noonTime) {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
+    fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        // Get and display temperature
+        let temp = data.list[noonTime].main.temp;
+        document.querySelector(`#temp-day-${day}`).textContent = `${temp}${degreeSymbol} F`;
+        // Get and display wind
+        let wind = data.list[noonTime].wind.speed;
+        document.querySelector(`#wind-day-${day}`).textContent = `${wind} MPH`
+        // Get and display humidity
+        let humidity = data.list[noonTime].main.humidity;
+        document.querySelector(`#humidity-day-${day}`).textContent = `${humidity}%`
+        // Get and display icon
+        let icon = data.list[noonTime].weather[0].icon;
+        const iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+        document.querySelector(`#icon-day-${day}`).setAttribute("src", iconurl)
+    })
+}
+
+const loadCurrentLocation = () => {
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(position => {
+            let lon = position.coords.longitude;
+            let lat = position.coords.latitude;
+            const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+            fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                // Display Current Date
+                getCurrentDate(0, "current-date", "long");
+                // Display Current City
+                document.querySelector("#current-city").textContent = "Current Location";
+                // Get current temperature
+                let currentTemp = data.current.temp;
+                currentTemp = currentTemp.toFixed(0);
+                document.querySelector("#current-temp").textContent = `${currentTemp}${degreeSymbol} F`;
+                // Get current wind
+                let currentWind = data.current.wind_speed;
+                document.querySelector("#current-wind").textContent = `${currentWind} MPH`
+                // Get current humidity
+                let currentHumidity = data.current.humidity;
+                document.querySelector("#current-humidity").textContent = `${currentHumidity}%`
+                // Get Icon
+                let icon = data.current.weather[0].icon;
+                const iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
+                document.querySelector("#current-icon").setAttribute("src", iconurl)
+                // Get and display current UV Index
+                let uvIndex = data.current.uvi;
+                const uvIndexEl = document.getElementById("current-uvIndex")
+                if (uvIndex < 4) {
+                    uvIndexEl.classList.add("label", "success");
+                    uvIndexEl.textContent = uvIndex;
+                } else if (uvIndex < 8) {
+                    uvIndexEl.classList.add("label", "warning");
+                    uvIndexEl.textContent = uvIndex;
+                } else {
+                    uvIndexEl.classList.add("label", "alert");
+                    uvIndexEl.textContent = uvIndex;
+                }
+            })
+        })
+    }
+}
+loadCurrentLocation();
+// Load buttons
+loadCities();
+// Click events and handlers
+buttonDivEl.addEventListener("click", buttonSearch)
 searchEl.addEventListener("click", getCurrentCityWeatherInfo);
 searchEl.addEventListener("click", forecast.bind(event, 1, 4));
 searchEl.addEventListener("click", forecast.bind(event, 2, 12));
 searchEl.addEventListener("click", forecast.bind(event, 3, 20));
 searchEl.addEventListener("click", forecast.bind(event, 4, 28));
 searchEl.addEventListener("click", forecast.bind(event, 5, 36));
-
-
-const findCity = function (city) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-    fetch(url)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data)
-            // Get current temperature
-            let currentTemp = data.main.temp;
-            currentTemp = currentTemp.toFixed(0);
-            document.querySelector("#current-temp").textContent = `${currentTemp}${degreeSymbol} F`;
-            // Get current wind
-            let currentWind = data.wind.speed;
-            document.querySelector("#current-wind").textContent = `${currentWind} MPH`
-            // Get current humidity
-            let currentHumidity = data.main.humidity;
-            document.querySelector("#current-humidity").textContent = `${currentHumidity}%`
-            // Get Icon
-            let icon = data.weather[0].icon;
-            const iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
-            document.querySelector("#current-icon").setAttribute("src", iconurl)
-            // Get current UV Index (first get lat and lon)
-            let lat = data.coord.lat;
-            let lon = data.coord.lon;
-            getUVIndex(lat, lon)
-            // Get city and save to local storage and display 
-            let cityName = data.name
-            document.querySelector("#current-city").textContent = cityName;
-            saveCity(cityName)
-        })
-}
-
-const getForecast = function (day, city, noonTime) {
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-            // Get and display temperature
-            let temp = data.list[noonTime].main.temp;
-            document.querySelector(`#temp-day-${day}`).textContent = `${temp}${degreeSymbol} F`;
-            // Get and display wind
-            let wind = data.list[noonTime].wind.speed;
-            document.querySelector(`#wind-day-${day}`).textContent = `${wind} MPH`
-            // Get and display humidity
-            let humidity = data.list[noonTime].main.humidity;
-            document.querySelector(`#humidity-day-${day}`).textContent = `${humidity}%`
-            // Get and display icon
-            let icon = data.list[noonTime].weather[0].icon;
-            const iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
-            document.querySelector(`#icon-day-${day}`).setAttribute("src", iconurl)
-        })
-}
